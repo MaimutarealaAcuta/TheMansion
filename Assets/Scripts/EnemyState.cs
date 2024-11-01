ac4ea7eae55924827f8ec0220d16bb24bf3000d1
IuliaCiof1 <iulia.ciofindac@gmail.com>
2024-11-01 17:53:50 +0200

    fixed layers and cleaned up Assets folder

--- Assets/Scripts/EnemyState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Base class for all enemy states
-public abstract class EnemyState : MonoBehaviour
-{
-    public abstract EnemyState RunCurrentState(EnemyManager enemy);
-}
